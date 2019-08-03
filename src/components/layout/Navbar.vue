288bf6af9a1e4e7f6d308cf97510dc669aaff434
rnsk <touch@rnsk.net>
2019-08-03 14:14:33 +0900

    ナビバーを追加、フォントアイコンを使用するためCDNリンクを追加

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <b-navbar type="is-danger">
+        <template slot="brand">
+            <b-navbar-item tag="div"><h1>VueJS DEMO</h1></b-navbar-item>
+        </template>
+        <template slot="start">
+            <b-navbar-item tag="router-link" to="/">
+                Home
+            </b-navbar-item>
+            <b-navbar-item tag="router-link" to="/about">
+                About
+            </b-navbar-item>
+        </template>
+
+        <template slot="end">
+            <b-navbar-item>
+                <b-icon
+                    pack="fab"
+                    icon="github">
+                </b-icon>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+</template>
