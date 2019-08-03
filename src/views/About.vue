6772418f5c2170e4ef6617db037dd03a3131dfa4
rnsk <touch@rnsk.net>
2019-08-03 15:39:25 +0900

    サイトの説明を追加

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <hr>
+    <p>VueJS の簡単なサンプル置き場です。<br>
+      ひたすらシンプルに書いています。</p>
+    <p>コードは Github のリポジトリを参照のこと。</p>
   </div>
 </template>
