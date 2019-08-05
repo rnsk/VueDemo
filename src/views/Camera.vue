29e7e4cced26b826ecc2463f8c091dd7c3de47f9
rnsk <touch@rnsk.net>
2019-08-05 15:28:02 +0900

    カメラの再生ボタンと停止ボタンを追加

--- src/views/Camera.vue
+++ src/views/Camera.vue
@@ -1,10 +1,29 @@
 <template>
     <div class="camera">
         <video ref="video" id="video" width="640" height="480" autoplay></video>
-        <p><button
+        <p>
+            <b-button
+            v-if="isPlaying"
+            @click="stop"
+            class="button is-warning"
+            icon-pack="fa"
+            icon-left="stop">Stop Video
+            </b-button>
+            <b-button
+            v-else
+            @click="start"
+            class="button is-info"
+            icon-pack="fa"
+            icon-left="play">Start Video
+            </b-button>
+            <b-button
+            v-if="isPlaying"
             @click="capture"
-            class="button is-success">Snap Photo
-            </button></p>
+            class="button is-link"
+            icon-pack="fa"
+            icon-left="camera"> Snap Photo
+            </b-button>
+        </p>
         <canvas ref="canvas" id="canvas" width="640" height="480"></canvas>
         <hr>
         <div class="columns">
@@ -31,26 +50,12 @@ export default {
         return {
             video: {},
             canvas: {},
-            captures: []
+            captures: [],
+            isPlaying: false
         }
     },
     mounted() {
-        this.video = this.$refs.video;
-        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-            navigator.mediaDevices.getUserMedia({
-                audio: false,
-                video: {
-                    facingMode: 'environment',
-                    // frameRate: { ideal: 5, max: 15 }// Firefox では指定できない
-                }
-            }).then(stream => {
-                this.video.srcObject = stream;
-                this.video.setAttribute('playsinline', true);// required to tell iOS safari we don't want fullscreen
-                this.video.play();
-            }).catch(error => {
-                console.log(error);
-            });
-        }
+        this.start()
     },
     methods: {
         capture() {
@@ -60,6 +65,39 @@ export default {
         },
         destroy(index) {
             this.captures.splice(index, 1)
+        },
+        start() {
+            this.video = this.$refs.video;
+            if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+                navigator.mediaDevices.getUserMedia({
+                    audio: false,
+                    video: {
+                        facingMode: 'environment',
+                        // frameRate: { ideal: 5, max: 15 }// Firefox では指定できない
+                    }
+                }).then(stream => {
+                    this.isPlaying = true;
+                    this.video.srcObject = stream;
+                    this.video.setAttribute('playsinline', true);// required to tell iOS safari we don't want fullscreen
+                    this.video.play();
+                }).catch(error => {
+                    console.log(error);
+                });
+            }
+        },
+        stop() {
+            this.video.pause();
+            let stream = this.video.srcObject;
+            if (stream != null) {
+                let tracks = stream.getTracks();
+
+                tracks.forEach(function (track) {
+                    track.stop();
+                });
+            }
+
+            this.video.srcObject = null;
+            this.isPlaying = false;
         }
     }
 }
